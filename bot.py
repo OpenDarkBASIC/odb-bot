26f2f0b8cdd6a95f1f9658cae9cf75ae6520e56f
TheComet <alex.murray@gmx.ch>
2020-12-10 17:04:21 +0100

    Implementing all relevant github events

--- bot.py
+++ bot.py
@@ -31,15 +31,138 @@ app = quart.Quart(__name__)
 
 
 def ping(data):
-    return "github ping"
+    return "(github ping)"
 
 def create(data):
+    ref_type = data["ref_type"]
+    ref = data["ref"]
+    user = data["sender"]["login"]
+    repo = data["repository"]["name"]
+
+    return f"{user} created {ref_type} `{ref}` for repo {repo}"
+
+def delete(data):
+    ref_type = data["ref_type"]
+    ref = data["ref"]
+    user = data["sender"]["login"]
+    repo = data["repository"]["name"]
+
+    return f"{user} deleted {ref_type} `{ref}` for repo {repo}"
+
+def issues(data):
+    action = data["action"]
+    number = data["issue"]["number"]
+    user = data["issue"]["user"]["login"]
+    title = data["issue"]["title"]
+
+    if action == "opened":
+        url = data["issue"]["html_url"]
+        return f"{user} opened new issue #{number} (<{url}>) ```{title}```"
+    elif action in ("reopened", "closed", "edited"):
+        url = data["issue"]["html_url"]
+        return f"{user} {action} issue #{number} (<{url}>) ```{title}```"
+    elif action == "deleted":
+        return f"{user} deleted issue #{number} ```{title}```"
+
+def issue_comment(data):
+    action = data["action"]
+    user = data["comment"]["user"]["login"]
+    url = data["issue"]["html_url"]
+    number = data["issue"]["number"]
+    title = data["issue"]["title"]
+
+    return f"{user} {action} comment on issue #{number} (<{url}>) ```{title}```"
+
+def push(data):
+    commit_msgs = [x["message"] for x in data["commits"]]
+    if len(commit_msgs) > 0:
+        commit_msgs = "\n".join(" * " + x for x in commit_msgs)
+        if len(commit_msgs) > 800:
+            return f"{data['pusher']['name']} pushed a whole bunch of shit to branch `{data['ref']}` (too much to list)"
+        else:
+            return f"{data['pusher']['name']} pushed to branch `{data['ref']}` ```\n{commit_msgs}```"
+    return f"{data['pusher']['name']} pushed to branch `{data['ref']}` ```\n * {data['head_commit']['message']}```"
+
+def fork(data):
+    user = data["sender"]["login"]
+    repo = data["repository"]["name"]
+    url = data["forkee"]["html_url"]
+
+    return f"{user} forked repo {repo} (<{url}>)"
+
+def commit_comment(data):
     return None
+
+def pull_request(data):
+    action = data["action"]
+    number = data["number"]
+    repo = data["repository"]["name"]
+    user = data["sender"]["login"]
+    url = data["pull_request"]["html_url"]
+    title = data["pull_request"]["title"]
+
+    map_action = {
+        "opened": "opened",
+        "edited": "edited",
+        "closed": "closed",
+        "review_requested": "requested a review on",
+        "review_request_removed": "removed request for a review on",
+        "ready_for_review": "is ready for review on",
+        "locked": "locked",
+        "unlocked": "unlocked",
+        "reopened": "reopened"
+    }
+
+    if action in ("labeled", "unlabeled", "synchronize", "assigned", "unassigned"):
+        return None
+
+    if action == "closed":
+        if data["pull_request"]["merged"]:
+            branch = data["pull_request"]["base"]["ref"]
+            return f"{user} merged PR #{number} into branch {branch} ```{title}```"
+
+    return f"{user} {map_action['action']} PR #{number} (<{url}>) ```{title}```"
+
+def pull_request_review(data):
+    return None
+
+def pull_request_review_comment(data):
+    return None
+
+def star(data):
+    action = data["action"]
+    repo = data["repository"]["name"]
+    user = data["sender"]["login"]
+    star_count = data["repository"]["stargazers_count"]
+
+    if action == "created":
+        return f"{user} starred repo {repo} ({star_count} stars total)"
+    if action == "deleted":
+        return f"{user} unstarred repo {repo} ({star_count} stars total)"
+
+def watch(data):
+    repo = data["repository"]["name"]
+    user = data["sender"]["login"]
+    watch_count = data["repository"]["watchers_count"]
+
+    return f"{user} watched repo {repo} ({watch_count} watches total)"
 
 
 github_handlers = {
     "ping": ping,
-    "create": create
+    "create": create,
+    "delete": delete,
+    "issues": issues,
+    "issue_comment": issue_comment,
+    "push": push,
+    "fork": fork,
+    "commit_comment": commit_comment,
+    "issue_comment": issue_comment,
+    "pull_request": pull_request,
+    "pull_request_review": pull_request_review,
+    "pull_request_review_comment": pull_request_review_comment,
+    "star": star,
+    "watch": watch
 }
 
 
@@ -61,12 +184,19 @@ async def hello():
     if channel is None:
         return ""
 
+    event_type = quart.request.headers["X-GitHub-Event"]
     try:
-        discord_msg = github_handlers[quart.request.headers["X-GitHub-Event"]](json.loads(payload.decode("utf-8")))
+        handler = github_handlers[event_type]
     except KeyError:
         # No handler for this event exists, so response with a generic one
-        discord_msg = "Unhandled github event: {}".format(quart.request.headers["X-GitHub-Event"])
+        print(f"No handler found")
+        await channel.send("Unhandled github event: `{}`".format(event_type))
+        return ""
+
+    try:
+        discord_msg = handler(json.loads(payload.decode("utf-8")))
     except Exception as e:
+        traceback.print_exc()
         discord_msg = f"```{e}```"
 
     if discord_msg is not None:
@@ -77,6 +207,15 @@ async def hello():
 @bot.command(name="ping")
 async def ping(ctx):
     await ctx.send("pong")
+
+
+@bot.command(name="echo")
+async def echo(ctx):
+    try:
+        msg = ctx.message.content.split(" ", 1)[1]
+    except:
+        return await ctx.send("Expected 1 argument")
+    await ctx.send(msg)
 
 
 loop = asyncio.get_event_loop()
